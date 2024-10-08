a57dd4112ef764ff6e399f7c3883c71f2933b066
Aditya Vr <adityaverma4768@gmail.com>
2024-10-08 12:28:43 +0530

    BRANCH

---
+++ Cpp_01.cpp
@@ -0,0 +1,55 @@
+
+//_______________Integer________________
+/*#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int num1,num2;
+    cout<<"Enter First Number: ";
+    cin>>num1;
+    cout<<"Enter Second Number: ";
+    cin>>num2;
+    cout<<"First Number is: "<<< num1 <endl;
+    cout<<"Second Number is: "<< num2 <<endl;
+
+}*/
+
+
+
+
+
+//________________Float and Double_____________________
+
+/*#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    float num1;
+    double num2;
+    cout<<"Enter Float Value: ";
+    cin>>num1;
+    cout<<"Enter Double Value: ";
+    cin>>num2;
+
+    cout<<"Float Value: "<< num1 <<endl;
+    cout<<"Double Value: "<< num2 <<endl;
+}*/
+
+
+
+
+
+//__________Char and String______________
+/*#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    char ch;
+    string str;
+    cout<<"Enter character: ";
+    cin>>ch;
+    cin.ignore();
+    cout<<"Enter String: ";
+    //If we use only cin to take input after space on that line it will not take anything so we use getline insted of cin
+    // cin>>str;
+    getline(cin,str);
+    cout<<"Character is: "<< ch <<endl;
+    cout<<"String is: "<< str << endl;
+}*/
---
+++ Cpp_02.cpp
@@ -0,0 +1,14 @@
+//________________If and Else___________________
+/*#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int age;
+    cout<<"Enter you age: ";
+    cin>>age;
+    if(age>18){
+        cout<<"You are allowed to vote."<<endl;
+    }
+    else{
+        cout<<"Your are not allowed.";
+    }
+}*/
